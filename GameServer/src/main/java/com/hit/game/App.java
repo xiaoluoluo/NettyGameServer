f5c50d147b0cdedbcae5d6587f955b281b1656b9
luoxiaosong <784478582@qq.com>
2016-11-17 17:43:30 +0800

    开天辟地创建maven工程

---
+++ GameServer/src/main/java/com/hit/game/App.java
@@ -0,0 +1,21 @@
+package com.hit.game;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		
+		logger.debug("111");
+//		logger.warn("1111");
+
+		// System.out.println("Hello World!");
+	}
+}
